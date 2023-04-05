ccac46b1a1db4aa69e7b928830da3cbd9852d376
JulioGM <martin.juliog@gmail.com>
2023-04-05 16:32:21 -0300

    Inclusion de database.json y actualizacion de .css general

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  const carousel = document.getElementById('Reel');
-  const images = carousel.querySelectorAll('img');
-  let index = 0;
-
-  function changeImage() {
-    images[index].classList.remove('active');
-    index = (index + 1) % images.length;
-    if (index < images.length) {
-      images[index].classList.add('active');
-    }
-  }
-
-  setInterval(changeImage, 3000); // cambia la imagen cada 3 segundos
-});
-
-function updateLabel() {
-  var precioInput = document.getElementById("precio");
-  var precioOutput = document.getElementById("precio-output");
-  var minPrecioOutput = document.getElementById("min-precio-output");
-  var maxPrecioOutput = document.getElementById("max-precio-output");
-
-  precioOutput.value = precioInput.value;
-  minPrecioOutput.textContent = precioInput.min;
-  maxPrecioOutput.textContent = precioInput.max;
-}
-
-
+const contenedorHTML = document.getElementById("contenedorCards")
+  fetch("./db/database.json")
+    .then(response => response.json())
+    .then(data => {
+        for(product of data){
+            contenedorHTML.innerHTML += `
+            <div class="cardbody">
+              <h4>${product.titulo}</h4>
+              <img src=${product.imagen}/>
+              <p>${product.descripcion}</p>
+              <p>Ubicacion: <strong>${product.ubicación}</strong></p>
+            `
+        }
+    })
+    .catch(error => console.log(error))

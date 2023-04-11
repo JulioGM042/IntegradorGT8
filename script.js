ddd4e0c58ff6c30f85f14310c1aaf925006056d0
JulioGM <martin.juliog@gmail.com>
2023-04-11 12:49:30 -0300

    ajutes de formulario en css y js

--- script.js
+++ script.js
@@ -6,10 +6,31 @@ const contenedorHTML = document.getElementById("contenedorCards")
             contenedorHTML.innerHTML += `
             <div class="cardbody">
               <h4>${product.titulo}</h4>
-              <img src=${product.imagen}/>
+              <img src="${product.imagen}"/>
               <p>${product.descripcion}</p>
               <p>Ubicacion: <strong>${product.ubicación}</strong></p>
             `
         }
     })
     .catch(error => console.log(error))
+
+    const formularioBusqueda = document.querySelector(".buscador")
+    formularioBusqueda.addEventListener("submit", (event) =>{
+      event.preventDefault()
+    })
+
+    const formularioContacto = document.querySelector(".contacto-form")
+    formularioContacto.addEventListener("submit", (event) =>{
+      event.preventDefault()
+    })
+    
+    const btnToast = document.getElementById("Enviarbtn")
+    btnToast.addEventListener("click", () =>{
+      Swal.fire({
+        title: 'Muchas gracias. Tu mensaje ha sido enviado',
+        icon: 'success',
+        color:'#F99417',
+        background: '#E8E2E2',
+      })
+  })
+
